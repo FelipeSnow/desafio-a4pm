799145b9b6a2cd5ffa0c3d6235b5bfab11d18ec4
FelipeSnow <felipenbrito18@outlook.com>
2024-09-17 21:23:41 -0300

    impl basic interface for login

--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,121 @@
-import React, { useState } from 'react';
-import { Text, TextProps, View, TextInput } from "react-native";
-
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Login() {
-  const [email, setEmail] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   return (
-    <View>
-      <View>
-        <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Olá,</Text>
-        <Text>Bem vindo de volta!</Text >
+    <SafeAreaView
+      style={{
+        backgroundColor: "white",
+        height: "100%",
+        width: "100%",
+        paddingHorizontal: 30,
+        alignSelf: "center",
+        display: "flex",
+      }}
+    >
+      <View style={{ marginTop: 30, marginBottom: 50 }}>
+        <Header />
       </View>
       <View>
-        <AccountInput label="Email" value={email} />
-        <AccountInput label="Senha" value={password} showValue={false} />
+        <AccountInput
+          onChangeText={(value) => {
+            setUsername(value);
+          }}
+          label="Nome de usuário"
+          value={username}
+        />
+        <AccountInput
+          onChangeText={(value) => {
+            setPassword(value);
+          }}
+          label="Senha"
+          value={password}
+          showValue={false}
+        />
+        <RegisterNav />
       </View>
-    </View>
+      <TouchableOpacity
+        onPress={() => Alert.alert("Não esquece de implementar o login")}
+      >
+        <View
+          style={{
+            backgroundColor: "lightblue",
+            padding: 20,
+            alignItems: "center",
+            borderRadius: 8,
+          }}
+        >
+          <Text style={{ fontSize: 18 }}>Entrar</Text>
+        </View>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
+
+const Header = () => (
+  <View>
+    <Text style={{ fontSize: 24, fontWeight: "bold" }}>Olá,</Text>
+    <Text>Bem vindo de volta!</Text>
+  </View>
+);
+
+const RegisterNav = () => (
+  <View>
+    <Text>Não tem uma conta?</Text>
+    <TouchableOpacity>
+      <Text>Criar conta</Text>
+    </TouchableOpacity>
+  </View>
+);
+
 interface AccountInputProps {
-  label: string
-  value: string
-  showValue?: boolean
+  label: string;
+  value: string;
+  showValue?: boolean;
+  onChangeText: (text: string) => void;
 }
 
-const AccountInput = ({ label, value, showValue = true }: AccountInputProps) => {
-  return <View>
-    <Text>{label}</Text>
-    <TextInput>{value}</TextInput>
-  </View>
-}
+const AccountInput = ({
+  label,
+  value,
+  showValue = true,
+  onChangeText,
+}: AccountInputProps) => {
+  return (
+    <View style={styles.Container}>
+      <Text style={{ fontSize: 18 }}>{label}</Text>
+      <TextInput
+        style={styles.TextBox}
+        secureTextEntry={!showValue}
+        value={value}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Container: {
+    flexDirection: "column",
+    //padding: 10,
+  },
+  TextBox: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderRadius: 5,
+    fontSize: 16,
+    borderColor: "#d9d9d9",
+    color: "black",
+    padding: 8,
+  },
+});
